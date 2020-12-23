2104b75c85974a4dde490e87f66bcd45f711dd7d
Yosuf Ibrahim <65606350+UsifFar@users.noreply.github.com>
2020-12-23 13:20:01 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'password',
+    'database' => 'key',
+    'recaptcha' => '6LcAphEaAAAAAPNADhYW0lVPzHvc3-gnDa3E6zDI'
+);
+?>
